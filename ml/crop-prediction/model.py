cbf9de9db3ddca3f3dba60111fafa12b8c601937
Tharun000g <tharun000g@gmail.com>
2025-11-08 20:18:18 +0530

    made a thing

---
+++ ml/crop-prediction/model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the dataset
+data = pd.read_csv('c:\\Users\\vikra\\OneDrive\\Documents\\Desktop\\Jeagerists\\ml\\crop-prediction\\Crop_recommendation.csv')
+
+# Display the first 5 rows of the dataset
+print(data.head())
